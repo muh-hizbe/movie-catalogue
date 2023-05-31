99af4f719e3a4a5e51b6c36575dc31538c48cca2
muh-hizbe <muh.saferagic@gmail.com>
2023-05-31 11:30:53 +0700

    Initialize movie catalogue automated testing implemented

---
+++ specs/helpers/testFactories.js
@@ -0,0 +1,12 @@
+import FavoriteMovieIdb from '../../src/scripts/data/favorite-movie-idb';
+import LikeButtonInitiator from '../../src/scripts/utils/like-button-presenter';
+     
+const createLikeButtonPresenterWithMovie = async (movie) => {
+  await LikeButtonInitiator.init({
+    likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    favoriteMovies: FavoriteMovieIdb,
+    movie,
+  });
+};
+ 
+export { createLikeButtonPresenterWithMovie };
